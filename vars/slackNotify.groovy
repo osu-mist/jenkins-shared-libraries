9fe99585e5c07cdf896ad70f11c98b1a436bdffe
ianbrown9475 <ian.brown9475@gmail.com>
2019-12-17 16:31:36 -0800

    Refactor using closure

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -11,6 +11,10 @@ class SlackNotifyConfig {
 }
 
 def call(def currentBuild, Map configMap = [:]) {
+  Closure<String> formatMessage = { String message ->
+    "${currentBuild.fullDisplayName} - ${message}"
+  }
+
   def config = configMap as SlackNotifyConfig
   echo "slackNotify library called with config: ${config.dump()}"
 
@@ -20,22 +24,21 @@ def call(def currentBuild, Map configMap = [:]) {
       && currentBuild.previousBuild.result != 'SUCCESS'
       && config.notifyBackToNormal
     ) {
-      echo "${currentBuild.fullDisplayName} - Back to normal"
-    }
-    else if (config.notifySuccess) {
-      echo "${currentBuild.fullDisplayName} - Success"
+      echo formatMessage("Back to normal")
+    } else if (config.notifySuccess) {
+      echo formatMessage("Success")
     }
   } else if (currentBuild.result == 'ABORTED' && config.notifyAborted) {
-    echo "${currentBuild.fullDisplayName} - Aborted"
+    echo formatMessage("Aborted")
   } else if (currentBuild.result == 'NOT_BUILT' && config.notifyNotBuilt) {
-    echo "${currentBuild.fullDisplayName} - Not built"
+    echo formatMessage("Not built")
   } else if (currentBuild.result == 'UNSTABLE' && config.notifyUnstable) {
-    echo "${currentBuild.fullDisplayName} - Unstable"
+    echo formatMessage("Unstable")
   } else if (currentBuild.result == 'FAILURE' && config.notifyFailure) {
     if (currentBuild.previousBuild?.result && currentBuild.previousBuild.result == 'SUCCESS') {
-      echo "${currentBuild.fullDisplayName} - Failure"
+      echo formatMessage("Failure")
     } else if (config.notifyRepeatedFailure) {
-      echo "${currentBuild.fullDisplayName} - Still failing"
+      echo formatMessage("Still failing")
     }
   }
 }
