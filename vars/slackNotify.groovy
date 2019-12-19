8a1fdf19d90283d66fb3e5e89fb6a3608dfa749b
ianbrown9475 <ian.brown9475@gmail.com>
2019-12-19 10:08:20 -0800

    Refactor colors

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -20,6 +20,12 @@ ${Util.getTimeSpanString(currentBuild.duration)} - ${message}"
 
   def config = configMap as SlackNotifyConfig
 
+  String successColor  = '#008000' // green
+  String abortedColor  = '#808080' // gray
+  String notBuiltColor = '#FF1493' // pink
+  String unstableColor = '#FFA500' // orange
+  String failureColor  = '#FF0000' // red
+
   if (currentBuild.result == 'SUCCESS') {
     if (
       currentBuild.previousBuild?.result
@@ -39,26 +45,26 @@ ${Util.getTimeSpanString(currentBuild.duration)} - ${message}"
       )
       slackSend(
         message: "${formatMessage("Back to normal")} after ${Util.getTimeSpanString(backToNormalTime)}",
-        color: '#008000'
+        color: successColor
       )
     } else if (config.notifySuccess) {
-      slackSend(message: formatMessage("Success"), color: '#008000')
+      slackSend(message: formatMessage("Success"), color: successColor)
     }
   } else if (currentBuild.result == 'ABORTED' && config.notifyAborted) {
-    slackSend(message: formatMessage("Aborted"), color: '#808080')
+    slackSend(message: formatMessage("Aborted"), color: abortedColor)
   } else if (currentBuild.result == 'NOT_BUILT' && config.notifyNotBuilt) {
-    slackSend(message: formatMessage("Not built"), color: '#FF1493')
+    slackSend(message: formatMessage("Not built"), color: notBuiltColor)
   } else if (currentBuild.result == 'UNSTABLE' && config.notifyUnstable) {
-    slackSend(message: formatMessage("Unstable"), color: '#FFA500')
+    slackSend(message: formatMessage("Unstable"), color: unstableColor)
   } else if (currentBuild.result == 'FAILURE' && config.notifyFailure) {
     if (
       currentBuild.previousBuild?.result
       && currentBuild.previousBuild.result == 'FAILURE'
       && config.notifyRepeatedFailure
     ) {
-      slackSend(message: formatMessage("Still failing"), color: '#FF0000')
+      slackSend(message: formatMessage("Still failing"), color: failureColor)
     } else {
-      slackSend(message: formatMessage("Failure"), color: '#FF0000')
+      slackSend(message: formatMessage("Failure"), color: failureColor)
     }
   }
 }
